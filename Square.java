4948a8286cd15ff95e79d8d7da546bde5e8f1c4a
Zach Schlotman <znet@ZnetServer.znet>
2018-09-13 19:18:59 -0500

    Initial commit

---
+++ Square.java
@@ -0,0 +1,39 @@
+public class Square{
+	public static final int TYPE_EMPTY = 0, TYPE_WALL = 1,TYPE_START = 2,TYPE_EMD = 3;
+	public static final char WORK_LIST = "o" , ALREADY_EXPLORED = ".", EXIT = "x";
+	private int row;
+	private int col;
+	private int type;
+	private char status;
+	public Square(int row,int col, int type){
+		this.row = row;
+		this.col = col;
+		this.type = type;
+		this.status = 0;
+	}
+	public String toString(){
+		if(type == 0){
+			if(status != 0){
+				return "" + status;
+			}
+			return "_";
+		}
+		switch(type){
+			case 1:
+				return "#";
+			case 2:
+				return "S";
+			case 3:
+				return "E";
+			default:
+				return "_";
+		}
+	}
+	boolean equals(Object o){
+		return ((Square)o).row == this.row && ((Square)o).col == this.col && ((Square)o).type == this.type && ((Square)o).status == this.status;
+	}
+	public int getRow(){return row;}
+	public int getCol(){return col;}
+	public int getType(){return type;}
+	public char getStatus(){return status;}
+}
